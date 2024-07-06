26e8625c184ade0c4198df7bd708f4a5726777fc
architKumar <https://github.com/architdhariwal>
2024-07-06 18:42:42 +0530

    first commit

---
+++ src/app.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const morgan = require('morgan');
+const cors = require('cors');
+const passport = require('./config/passport');
+const authRoutes = require('./routes/authRoutes');
+const studentRoutes = require('./routes/studentRoutes');
+
+const app = express();
+
+app.use(express.json());
+app.use(morgan('dev'));
+app.use(cors());
+app.use(passport.initialize());
+
+app.use('/api/auth', authRoutes);
+app.use('/api/students', studentRoutes);
+
+app.use((err, req, res, next) => {
+  console.error(err.stack);
+  res.status(500).json({ error: 'Internal Server Error' });
+});
+
+module.exports = app;
